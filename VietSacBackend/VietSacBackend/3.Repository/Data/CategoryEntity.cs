3b6e9541ac103bed99df72c7033ed444a63136d5
BaoBiBo <ng.hoangbao03@gmail.com>
2024-06-05 03:46:25 +0700

    Init

---
+++ VietSacBackend/VietSacBackend/3.Repository/Data/CategoryEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace VietSacBackend._3.Repository.Data
+{
+    public class CategoryEntity : Entity
+    {
+        public string category_name { get; set; }
+        public virtual ICollection<ProductEntity> Products { get; set; }
+    }
+}
---
+++ VietSacBackend/VietSacBackend/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using VietSacBackend._3.Repository.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddDirectoryBrowser();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Cors
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("corspolicy", build =>
+    {
+        build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+//DB SQL Server
+builder.Services.AddDbContext<VietSacContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("VietSac"));
+});
+
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+//Authentication
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+        (builder.Configuration["JWT:Secret"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
